296dba8b8774716acba40cefaa5b19442cbfc4e5
Chen Hao <823147833@qq.com>
2019-05-11 13:21:37 +0800

    update

---
+++ precache-manifest.024e98a53c0adb01d5429b63e0f669a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c4755a7e048d15362bd8e1081ec1f61",
+    "url": "./static/media/student.9c4755a7.jpg"
+  },
+  {
+    "revision": "ad4eb2987149780c2c92e9e2a2b71371",
+    "url": "./static/media/school_score.ad4eb298.jpg"
+  },
+  {
+    "revision": "f44978b7ac01c6ee2061c116da41155c",
+    "url": "./static/media/class.f44978b7.jpg"
+  },
+  {
+    "revision": "af3eccc83157d84825b06a050c7a97fb",
+    "url": "./static/media/app.af3eccc8.jpg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "0de0c46d2ae5d7eb0fb1",
+    "url": "./static/js/main.1a82af11.chunk.js"
+  },
+  {
+    "revision": "1a4167970ca58c1d647a",
+    "url": "./static/js/2.9a123123.chunk.js"
+  },
+  {
+    "revision": "0de0c46d2ae5d7eb0fb1",
+    "url": "./static/css/main.01a5057a.chunk.css"
+  },
+  {
+    "revision": "1a4167970ca58c1d647a",
+    "url": "./static/css/2.8928bfc8.chunk.css"
+  },
+  {
+    "revision": "a9e3e7c4e60dc4a815629366e4e3bd66",
+    "url": "./index.html"
+  }
+];
