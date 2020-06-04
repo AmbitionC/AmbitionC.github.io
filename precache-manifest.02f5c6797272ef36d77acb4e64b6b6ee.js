4845a343345ea41d6b4770102fa709d651c98410
Chen Hao <823147833@qq.com>
2020-06-04 15:55:01 +0800

    feat: initial

---
+++ precache-manifest.02f5c6797272ef36d77acb4e64b6b6ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8cc8658cfa5d504e59ebc8ab1f1ea9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df556d214608d930a104",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "22daff2d5817dcedf5f7",
+    "url": "/static/js/2.136eb3b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.136eb3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df556d214608d930a104",
+    "url": "/static/js/main.873d75e7.chunk.js"
+  },
+  {
+    "revision": "ae3ddae1838463c68e2c",
+    "url": "/static/js/runtime-main.72076a54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
